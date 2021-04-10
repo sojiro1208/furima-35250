2b939c5e781cd439aa600e7543fb5267be9c4817
sojiro1208 <80874997+sojiro1208@users.noreply.github.com>
2021-04-10 21:53:36 +0900

    バリデーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validates :first_name,      presence: true
-  validates :last_name,       presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/ } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
   validates :nickname,        presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana,  presence: true
   validates :birthday,        presence: true
+  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: "Include both letters and numbers"
 
   # has_many :products
   # has_many :purchases
