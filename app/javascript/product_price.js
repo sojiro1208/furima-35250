16fd6c12ec0fd4d5b2956ab663d753590d8b007d
sojiro1208 <80874997+sojiro1208@users.noreply.github.com>
2021-04-13 11:01:54 +0900

    販売価格などの表示

--- app/javascript/product_price.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', () => {
-  const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-   console.log("イベント発火");
-})
-});
