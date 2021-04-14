d4fcbcb4aedd438d98908e522889912a241029bc
sojiro1208 <80874997+sojiro1208@users.noreply.github.com>
2021-04-14 19:05:39 +0900

    商品編集機能

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,6 +25,20 @@ class ProductsController < ApplicationController
       @product = Product.find(params[:id])
     end
 
+    def edit
+      @product = Product.find(params[:id])
+    end
+
+    def update
+      @product = Product.find(params[:id])
+      if
+        @product.update(product_params)
+        redirect_to product_path(@product.id)
+      else
+        render :edit
+      end
+    end
+
     private
     def product_params
       params.require(:product).permit(:name, :category_id, :status_id, :shipping_id, :area_id, :delivery_day_id, :price, :information, :image).merge(user_id: current_user.id)
